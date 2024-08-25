4549bab37af83bd28109c553fef0f2c9f81d6c53
Marlonn Locatelli <marlonn@gmail.com>
2024-08-25 16:24:12 -0300

    To-do list e lógica

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,108 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive } from 'vue';
+
+const estado = reactive ({
+    // all, pending, finished
+    filtro: 'all',
+    tempTask: '',
+    tarefas: [
+        {
+            titulo: 'Comprar melão',
+            finalizada: false,
+        },
+        {
+            titulo: 'Lavar cuecas',
+            finalizada: false,
+        },
+        {
+            titulo: 'Plantar horta',
+            finalizada: true,
+        },
+    ]
+})
+
+const getOnGoingTasks = () => {
+    // retorna apenas as tarefas com false
+    return estado.tarefas.filter(tarefa => !tarefa.finalizada);
+}
+
+const getFinishedTasks = () => {
+    // retorna apenas as tarefas com false
+    return estado.tarefas.filter(tarefa => tarefa.finalizada);
+}
+
+const getFilteredTasks = () => {
+    const { filtro } = estado;
+
+    switch (filtro) {
+        case 'pending':
+            return getOnGoingTasks();
+
+        case 'finished':
+            return getFinishedTasks();
+
+        default:
+            return estado.tarefas;
+    }
+}
+
+const addTask = () => {
+    estado.tarefas.push({
+        titulo: estado.tempTask,
+        finalizada: false,
+    })
+
+    estado.tempTask = '';
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="container">
+        <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+            <h1>Tarefas</h1>
+            <p>
+                {{ getOnGoingTasks().length }} tarefas pendentes
+            </p>
+        </header>
+
+        <form @submit.prevent="addTask">
+            <div class="row">
+
+                <div class="col">
+                    <input :value="estado.tempTask" @change="evento => estado.tempTask = evento.target.value" required class="form-control" type="text" placeholder="Descrição da tarefa">
+                </div>
+
+                <div class="col-md-1">
+                    <button class="btn btn-primary" type="submit">Cadastrar</button>
+                </div>
+
+                <div class="col-md-2">
+                    <select @change="evento => estado.filtro = evento.target.value" class="form-control">
+                        <option value="all">Todas as tarefas</option>
+                        <option value="pending">Pendentes</option>
+                        <option value="finished">Finalizadas</option>
+                    </select>
+                </div>
+            </div>
+        </form>
+
+        <ul class="list-group mt-4">
+            <li v-for="tarefa in getFilteredTasks()" class="list-group-item">
+                <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada" :id="tarefa.titulo" type="checkbox">
+                <label :class="{ finished: tarefa.finalizada }" :for="tarefa.titulo" class="ms-2">
+                    {{ tarefa.titulo }}
+                </label>
+            </li>
+        </ul>
+
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+    .finished {
+        text-decoration: line-through;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+
+// importação do bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createApp(App).mount('#app')
